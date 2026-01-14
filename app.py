91d01ec92aa5cc014bfba042e502f52b95d5130c
Marta Mysyshyn <martamysyshyn27@gmail.com>
2026-01-14 20:22:35 +0200

    Created Sign Up Page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home_page.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('sign_up_page.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
