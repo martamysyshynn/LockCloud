4da739b9ee679d256342de42c2f79cbd40725687
Marta Mysyshyn <martamysyshyn27@gmail.com>
2026-01-15 20:36:11 +0200

    Remove config.py from repository

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = "7e105b5ffa5311a0019a6635c4185b61ea6a9f6bde8f86a939b246dbf3716570"  
-
-    MYSQL_HOST = "localhost"          
-    MYSQL_USER = "root"              
-    MYSQL_PASSWORD = "MarTa#08/27"   
-    MYSQL_DB = "cloud_storage"     
